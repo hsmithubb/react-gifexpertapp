fe13c9e03317d18fbb90cf72260e6c3ac3da5cdc
Hugo <hsmith.ubb@gmail.com>
2020-09-14 01:45:23 -0300

    docs agregado

---
+++ docs/precache-manifest.3738f5620b02dfe9adc88d47573ce7c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db25c1cdf6df62908990b0dec0bfa439",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5dd015b0ad3188919c7",
+    "url": "/static/css/main.8f951f21.chunk.css"
+  },
+  {
+    "revision": "92a5aaf936af7a3a2930",
+    "url": "/static/js/2.7c8d2b56.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7c8d2b56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5dd015b0ad3188919c7",
+    "url": "/static/js/main.dd9513b0.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
